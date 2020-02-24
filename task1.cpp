7986365b17fbbd56f94e704a269f0b85adf7b983
Hung150 <55206412+Hung150@users.noreply.github.com>
2020-02-24 09:13:18 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>  
+#include <string>
+using namespace std;
+ 
+int main()
+{
+ int maxint(int [], int);
+ int numbers [100];
+ int n=0;
+ char ans;
+ do
+ {
+  cout << "\nEnter number: "; cin >> numbers[n++];
+  cout << "\nContinue typing? "; cin >> ans;
+ }
+ while (ans != 'n');
+ cout << "\ngreatest value: " << numbers[maxint(numbers, n)] << ", index into the array: " << maxint(numbers, n) << endl;
+
+ system ("pause");
+ return 0;
+}
+int maxint(int num [], int n)
+{
+ int max=0, index=0;
+
+ for (int j=0; j<n; ++j)
+ {
+  if (num[j]>max)
+  {
+   max = num[j];
+   index = j;
+  }
+ }
+  return index;
+}
